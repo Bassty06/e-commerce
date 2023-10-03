6443151b0faf27cd40a9ef36f89683c29b742651
“sebastian <asebastian090@gmail.com>
2023-10-03 18:27:01 -0500

    base de proyecto

---
+++ src/navigation/Navigation.js
@@ -0,0 +1,39 @@
+import React from "react";
+import Favorites from "../screens/Favorites";
+import Pokedex from "../screens/Pokedex";
+import Account from "../screens/Account";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Icon from "react-native-vector-icons/FontAwesome";
+
+const Tap = createBottomTabNavigator();
+
+export default function Navigation() {
+  return (
+    <Tap.Navigator>
+      <Tap.Screen
+        name="Favorites"
+        component={Favorites}
+        options={{
+          tabBarLabel: "Favorites",
+          tabBarIcon: () => <Icon name="heart" size={20} color="#000" />,
+        }}
+      />
+      <Tap.Screen
+        name="Pokedex"
+        component={Pokedex}
+        options={{
+          tabBarLabel: "Pokedex",
+          tabBarIcon: () => <Icon name="globe" size={60} color="#000" />,
+        }}
+      />
+      <Tap.Screen
+        name="Account"
+        component={Account}
+        options={{
+          tabBarLabel: "Account",
+          tabBarIcon: () => <Icon name="user" size={20} color="#000" />,
+        }}
+      />
+    </Tap.Navigator>
+  );
+}
---
+++ src/screens/Carros.js
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function Carros() {
+  return (
+    <View>
+      <Text>estamos en un Carro</Text>
+    </View>
+  );
+}
